17292d7ecdcc4c3fbba35ee2813128739a4e6433
supakitwongkaew <supakit.wo.61@ubu.ac.th>
2020-09-30 09:51:07 +0700

    Updates

---
+++ precache-manifest.46c0c1f22af910514c9c1040c825a247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362e8592a2807336ea05ca9f1f21388a",
+    "url": "/CmProject/index.html"
+  },
+  {
+    "revision": "c5a8e6e3ccca48c7c965",
+    "url": "/CmProject/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "73b1210f1e4b25dff3f4",
+    "url": "/CmProject/static/js/2.64720222.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CmProject/static/js/2.64720222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a8e6e3ccca48c7c965",
+    "url": "/CmProject/static/js/main.bce63ac6.chunk.js"
+  },
+  {
+    "revision": "43f7f335a1aba4e82c69",
+    "url": "/CmProject/static/js/runtime-main.6ef87d22.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/CmProject/static/media/logo.ee7cd8ed.svg"
+  }
+]);
